a2f1b3170d855809ef1fcd2ff227ba67da951281
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-08-30 13:32:12 +0700

    Integration template admin

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('content')
+<div class="section-body">
+  <p>Hello World!</p>
+</div>
+@endsection
+
+@push('page-scripts')
+
+@endpush
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    Copyright &copy; 2023
+  </div>
+  <div class="footer-right">
+    1.0.0
+  </div>
+</footer>
